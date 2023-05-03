4e3e5df38acd6e19a8bfd6e32e3dedce58af7f03
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-02 21:56:28 -0500

    AJAX - XMLHttpRequest

--- 16 - Asynchronous/starter/Notes.js
+++ 16 - Asynchronous/starter/Notes.js
@@ -24,18 +24,37 @@ Assigning a .src attribute to an image is does asynchronously because in the bac
 
 Only the .src by itself is async.
 
-AJAX = Asynchronous Javascript And Xml: Ajax calls allow us to request data from remote servers asynchronously. We reveive data in the background without blocking the main thread of execution.
+AJAX = Asynchronous Javascript And Xml - JSON: Ajax calls allow us to request data from remote servers asynchronously. We reveive data in the background without blocking the main thread of execution.
 
+With AJAX we make an HTTP request to a server for some data, the server will send back the response with the requested data. This all happens asynchronously. There can be get requests to get data or there can be set requests to send information to a server.
 
+A web API is Application Programming Interface. It's a piece of software that may be utilized by another API or software to allow applications to talk with each other to set/get data. - THIS IS TRUE FOR ALL OF PROGRAMMING
 
+- DOM API - GeoLocation API - Leaflet API -
 
+--- Own class API (Objects made from a class are self contained pieces of software that allows data to be transferred) --- 
 
+-- ONLINE API (App. running on a web server that receives requests for data and sends back data it has retrieved from some database or some other type of API as a response)--- Called just plainly 'API' or Web API
 
+ JSON is the most popular data format today. It's basically just a javascript object converted to a string. Therefore it's easy to send across the web JSON.stringify(object) will turn a string of data into a javascript object and JSON.parse(`string`) will convert a javascript string to an object. 
 
+ There are API's for EVERYTHING. GitHub has a public-apis repo created to store hundreds of APIs used for almost anything. 
 
+ An API endpoint is another name for the URL related to the API.
 
+  With the old method of XMLHttpRequest, we have to add an event handler to listen to the request in order to load the data once it has finsihed loading. This is all async. We then use the .responseText property to access the property on the object that contains the response from the API. This response is in JSON format, a formatting method of sending data across APIs. 
 
+// Multiple ways of doing AJAX calls
+// 1.) Create the variable containing the XMLHttpRequest() AJAX call - OLD SCHOOL WAY
 
+const request = new XMLHttpRequest();
+
+// 2.) Use the .open() method on the request object and specify the first param as `SET` or `GET`
+request.open(`GET`, `https://restcountries.com/v3.1/alpha/US`);
+
+// 3.) Send off the request to the desired URL
+// This is all done asynchronously in the background
+// 4.) We then add an event listener to listen for this data to load, only then can we access this data.
 
 
 
