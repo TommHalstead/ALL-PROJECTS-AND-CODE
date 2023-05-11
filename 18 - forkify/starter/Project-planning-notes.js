ba0a23dab2b735b9dc93bd068fe1033864d8fc60
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-11 10:39:41 -0500

    npm init on forkify project

---
+++ 18 - forkify/starter/Project-planning-notes.js
@@ -0,0 +1,148 @@
+/*
+
+USER STORIES: 
+
+1.) As a user, I want to SEARCH FOR RECIPES, so that I can find NEW IDEAS FOR MEALS
+2.) As a user, I wasnt to be able to UPDATE THE NUMBER OF SERVINGS, so that I can cook a meal for DIFFERENT NUMBER OF PEOPLE 
+3.) As a user, I want to BOOKMARK RECIPES, so that I can REVIEW THEM LATER
+4.) As a user, I want to be able to CREATE MY OWN RECIPES, so that I have them all ORGANIZED IN THE SAME APP
+5.) As a user, I want to be able to SEE MY BOOKMARKS AND RECIPES SO THAT I CAN VIEW THEM LATER ONCE I CLOSE THE APP.
+
+
+FEATURES: 
+
+        User-1 - Search for items
+1.) Search functionality: Input fields to send request to API with searched keywords
+2.) Display results with multiple pages (pagination) for multiple results.
+3.) Display recipe with cooking time, servings and ingredients 
+
+        User-2 - Update servings
+4.) Change servings functionality: Update all ingredients according th current number of servings
+
+        User-3 - Bookmark recipes
+5.) Bookmarking functionality: Display list of all bookmarked ingredients
+
+        User-4 - Create recipes
+6.) User can upload their own recipes
+7.) User own recipes will automatically be bookmarked
+8.) User can only see their own recipes, not recipes from other users.
+
+        User-5 - See my bookmarks and recipes and come back later
+9.) Store bookmark data in the browser using local storage.
+10.) On page load, read saved bookmarks from local storage and display them on the page.
+
+When working with your package.json file, a devDependency is an element that contains all the packages that your prject requires in the development process and not in the prodcution or testing environments/phases.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
